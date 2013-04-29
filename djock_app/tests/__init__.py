7a8f4e399b2883d31763a29164956f08340d826d
intramigo <me>
2013-04-29 14:16:43 -0500

    -

--- djock_app/tests/__init__.py
+++ djock_app/tests/__init__.py
@@ -1,5 +1,5 @@
-# from djock_app.tests.BLAH import *
-# from djock_app.tests.ANOTHER_BLAH import *
-from djock_app.tests.lock_comm import *
-from djock_app.tests.functional import *  # i.e. not unit tests 
-from djock_app.tests.additional_tests import *  # todo
+# In order to avoid running all tests, import and specify on command line, e.g. 
+# $ ./manage.py test djock_app.DoorModelTests
+from models_tests import DoorModelTests
+from admin_tests import LockUserFormTests
+from functional_tests import RenameLaterFunctionalTests
